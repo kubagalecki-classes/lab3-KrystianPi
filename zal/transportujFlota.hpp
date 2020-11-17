c404aba7f835969f6416318e6366658f5b54f9da
KrystianPi <krystek.pietrzak@gmail.com>
2020-11-17 14:32:29 +0000

    v3

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -7,11 +7,11 @@ unsigned int transportujFlota(unsigned int towar)
     if(towar = 0){
        return 0;
     }else{
-      Stocznia stocznia{};
-      Statek* s1 = stocznia(); 
       int przewieziony_towar = 0; 
       int liczba_zaglowcow = 0;
       while(przewieziony_towar<towar){
+        Stocznia stocznia{};
+        Statek* s1 = stocznia(); 
         przewieziony_towar = s1->transportuj()+przewieziony_towar; 
         if( s1 == dynamic_cast<Zaglowiec*>(s1)){
             liczba_zaglowcow ++; 
